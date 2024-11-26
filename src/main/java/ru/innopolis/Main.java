281c6e0b9ce2422e0875e808179cd33853f77666
Sh.Sergei <duser87@yandex.ru>
2024-11-26 22:42:37 +0300

    Test №3

--- src/main/java/ru/innopolis/Main.java
+++ src/main/java/ru/innopolis/Main.java
@@ -16,6 +16,10 @@ public class Main {
 
         products.add(new Product(3, "This is the No.3 product", 20L, 40L));
 
+        products.add(new Product(4, "This is the No.4 product", 30L, 50L));
+
+        products.add(new Product(5, "This is the No.5 product", 40L, 60L));
+
         for(Product p: products){
 
             System.out.println(p.toString());
